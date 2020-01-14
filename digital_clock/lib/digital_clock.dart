506bba3f2dd1331dbe64ee07d77aa7e122cad48e
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2020-01-14 19:23:45 +0800

    Add seconds update

--- digital_clock/lib/digital_clock.dart
+++ digital_clock/lib/digital_clock.dart
@@ -86,18 +86,18 @@ class _DigitalClockState extends State<DigitalClock> {
       _dateTime = DateTime.now();
       // Update once per minute. If you want to update every second, use the
       // following code.
-      _timer = Timer(
-        Duration(minutes: 1) -
-            Duration(seconds: _dateTime.second) -
-            Duration(milliseconds: _dateTime.millisecond),
-        _updateTime,
-      );
-      // Update once per second, but make sure to do it at the beginning of each
-      // new second, so that the clock is accurate.
       // _timer = Timer(
-      //   Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
+      //   Duration(minutes: 1) -
+      //       Duration(seconds: _dateTime.second) -
+      //       Duration(milliseconds: _dateTime.millisecond),
       //   _updateTime,
       // );
+      // Update once per second, but make sure to do it at the beginning of each
+      // new second, so that the clock is accurate.
+      _timer = Timer(
+        Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
+        _updateTime,
+      );
     });
   }
 
@@ -110,6 +110,7 @@ class _DigitalClockState extends State<DigitalClock> {
     final hour =
         DateFormat(widget.model.is24HourFormat ? 'HH' : 'hh').format(_dateTime);
     final minute = DateFormat('mm').format(_dateTime);
+    final seconds = DateFormat('ss').format(_dateTime);
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
 
@@ -153,6 +154,15 @@ class _DigitalClockState extends State<DigitalClock> {
                 ClockNumber(
                   rootWidth: width,
                   number: int.parse(minute[1]),
+                ),
+                SizedBox(width: 32),
+                ClockNumber(
+                  rootWidth: width,
+                  number: int.parse(seconds[0]),
+                ),
+                ClockNumber(
+                  rootWidth: width,
+                  number: int.parse(seconds[1]),
                 ),
               ],
             ),
